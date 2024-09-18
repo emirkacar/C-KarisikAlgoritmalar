2911a25d335b820a37ca3d42cfb8b9870a6b96e9
emirkacar <emirkacar86@gmail.com>
2024-09-18 19:44:07 +0300

    Add files via upload

--- sayininTersi.c
+++ sayininTersi.c
@@ -1,30 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
 int main()
 {
-    int sayi,i,kalan;
-    int gecici;
-    int tersSayi=0;
-
-    printf("Sayiyi giriniz: ");
+    int sayi,kalan,sayininTersi=0,sayac=0,gecici;
+    printf("Sayi: ");
     scanf("%d",&sayi);
+    gecici=sayi;
+    while(gecici!=0)
+    {
+        sayac+=1;
+        gecici/=10;
+    }
+    printf("Orijinal sayi = %d\n",sayi);
+    printf("Sayac = %d\n",sayac);
+    while(sayi!=0)
+    {
+        kalan=sayi%10;
+        sayininTersi+=kalan*pow(10,sayac-1);
+        sayi/=10;
+        sayac--;
+    }
+    printf("Sayinin tersi = %d",sayininTersi);
 
-    if(sayi>=0)
-    {
-        gecici=sayi;
-        while(gecici!=0)
-        {
-            kalan=gecici%10;
-            tersSayi=tersSayi*10+kalan;
-            gecici/=10;
-            i++;
-        }
-        printf("%d sayisinin tersi = %d",sayi,tersSayi);
-    }
-    else
-    {
-        printf("Negatif sayilarin tersi alinamaz.");
-    }
     return 0;
 }
